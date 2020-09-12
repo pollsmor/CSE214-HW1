813d1b6ff5582526c9e1c24b3530e33813eae0c2
Kevin Li <kevinli4321@gmail.com>
2020-09-12 12:45:34 -0400

    Merge function

--- src/cse214hw1/Tests.java
+++ src/cse214hw1/Tests.java
@@ -6,13 +6,19 @@ public class Tests {
     public static void main(String[] args) {
         System.out.println("Rotate tests");
         int[] a = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(a));
         ArrayUtils.rotate(a, 1);
 
         int[] b = {1, 2, 3, 4, 5};
         ArrayUtils.rotate(b, 2);
 
+        char[] c = {'a', 'b', 'c', 'd', 'e'};
+        ArrayUtils.rotate(c, 2);
+
         System.out.println(Arrays.toString(a));
         System.out.println(Arrays.toString(b));
+        System.out.println(Arrays.toString(c));
+
+        System.out.println("Merge test");
+        System.out.println(Arrays.toString(ArrayUtils.merge(a, b)));
     }
 }
